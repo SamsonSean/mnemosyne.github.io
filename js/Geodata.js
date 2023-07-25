a52a30762b912a57c3726bab1a2800e7db09f247
SamsonSean <samsonsean.10@gmail.com>
2023-07-25 14:18:29 +0800

    Added JWT token

--- js/Geodata.js
+++ js/Geodata.js
@@ -1,44 +1,33 @@
-
-function loadData() {
-
-    fetch('https://rafmesdev.philmech.gov.ph/netapi/meme',
-        {
-            method: "GET",
+async function FetchOwners(){
+    try {
+      const tokenn = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIwMjAxMTIwMzExMTE1MjMiLCJuYW1lIjpbIlNFQU4uRU5DT0RFUiIsIlNFQU4uRU5DT0RFUiJdLCJVc2VybmFtZSI6InNlYW4uZW5jb2RlciIsInJvbGUiOiJFTkNPREVSIiwiQ29udHJvbF9BcmVhIjoiMDM0OTAxMDAwIiwibmJmIjoxNjkwMjY1NzM1LCJleHAiOjE3MjE4ODgxMzUsImlhdCI6MTY5MDI2NTczNSwiaXNzIjoiaHR0cHM6Ly9yYWZtZXNkZXYucGhpbG1lY2guZ292LnBoIn0.MmZkB0hyXTtaPjbPJKQUefdxoyC9vWA-ltDJghVr814'
+      fetch('https://rafmesdev.philmech.gov.ph/netapi/Owners', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': `Bearer ${tokenn}`
+        }
+      })
+        .then((response) => {
+          if (!response.ok) {
+            alert("Bad Request")
+          } else if (response.status === 401) {
+            alert("Error fetching");
+          }
+          return response.json();
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new error("Can't fetch data");
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
+        .then((json) => {
+          var arr = json;
+        
+          console.log(arr)
         })
         .catch(error => {
-            console.error('Error fetching data:', error);
-        });;
-}
+          alert(error);
+        })
+        
+    } catch (error) {
+      alert(error);
+    }
+  }
 
-// function LoadAbemisData(data) {
-//     $('#abemis-table').DataTable().destroy();
-//     $('#abemis-table').DataTable({
-//         "data": data,
-//         "lengthChange": false,
-//         'searching': false,
-//         'paging': false,
-//         "columns": [
-//             { "data": "machine_name" },
-//             { "data": "brand" },
-//             { "data": "serial_number" },
-
-//             {
-//                 "data": "id",
-//                 "render": function (data, type, row, meta) {
-//                     return '<a href="/owners.machines-add.asp?u=' + OwnerId + '&r=' + encodeURIComponent(data) + '" class="btn btn-success">Register</a>';
-//                 }
-//             }
-//         ]
-//     });
-// }
-
-loadData();
+  FetchOwners();
