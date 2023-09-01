22b93b2d06f660d8315c7b485044d582957ba537
SamsonSean <samsonsean.10@gmail.com>
2023-09-01 16:02:01 +0800

    Save The Date Marvin

--- js/Geodata.js
+++ js/Geodata.js
@@ -1,12 +1,11 @@
 async function FetchOwners(){
     try {
-      const tokenn = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIwMjAxMTIwMzExMTE1MjMiLCJuYW1lIjpbIlNFQU4uRU5DT0RFUiIsIlNFQU4uRU5DT0RFUiJdLCJVc2VybmFtZSI6InNlYW4uZW5jb2RlciIsInJvbGUiOiJFTkNPREVSIiwiQ29udHJvbF9BcmVhIjoiMDM0OTAxMDAwIiwibmJmIjoxNjkwMjY1NzM1LCJleHAiOjE3MjE4ODgxMzUsImlhdCI6MTY5MDI2NTczNSwiaXNzIjoiaHR0cHM6Ly9yYWZtZXNkZXYucGhpbG1lY2guZ292LnBoIn0.MmZkB0hyXTtaPjbPJKQUefdxoyC9vWA-ltDJghVr814'
-      fetch('https://rafmesdev.philmech.gov.ph/netapi/Owners', {
+      fetch('https://localhost:7172/api/Home/Heroes', {
         method: 'GET',
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Authorization': `Bearer ${tokenn}`
-        }
+          'Content-Type': 'application/json',
+          // 'Authorization': `Bearer ${tokenn}`
+      }
       })
         .then((response) => {
           if (!response.ok) {
@@ -18,16 +17,15 @@ async function FetchOwners(){
         })
         .then((json) => {
           var arr = json;
-        
+          
+          LoadAbemisData(arr);
           console.log(arr)
         })
         .catch(error => {
           alert(error);
-        })
-        
+        })     
     } catch (error) {
       alert(error);
     }
   }
-
   FetchOwners();
